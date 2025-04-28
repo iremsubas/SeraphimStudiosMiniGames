3a5707962e316288a14b95e37aeb5109260e7f3b
zixinq <107233955+zixinq@users.noreply.github.com>
2025-04-28 13:41:51 -0400

    text panels

--- Assets/Scripts/InkStoryManager.cs
+++ Assets/Scripts/InkStoryManager.cs
@@ -4,137 +4,137 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-
-// This is a super bare bones example of how to play and display a ink story in Unity.
-public class InkStoryManager : MonoBehaviour {
+public class InkStoryManager : MonoBehaviour
+{
     public static event Action<Story> OnCreateStory;
-	
-    void Awake () {
-		// Remove the default message
-		RemoveChildren();
-		StartStory();
-	}
 
-	// Creates a new Story object with the compiled story which we can then play!
-	void StartStory () {
-		story = new Story (inkJSONAsset.text);
-		if (PuttingCondom.nextKnot != "") 
-		{
-			Debug.Log("Choosing path: " + PuttingCondom.nextKnot);
-			story.ChoosePathString(PuttingCondom.nextKnot);
-			PuttingCondom.nextKnot = ""; // Clear it after using
-		}
+    [Header("Ink Story")]
+    [SerializeField] private TextAsset inkJSONAsset = null;
+    private Story story;
 
-		if (OnCreateStory != null) 
-			OnCreateStory(story);
-		
-		RefreshView();
-	}
-	
-	// This is the main function called every time the story changes. It does a few things:
-	// Destroys all the old content and choices.
-	// Continues over all the lines of text, then displays all the choices. If there are no choices, the story is finished!
-	void RefreshView () {
-		// Remove all the UI on screen
-		RemoveChildren ();
-		
-		// Read all the content until we can't continue any more
-		while (story.canContinue) {
-			// Continue gets the next line of the story
-			string text = story.Continue ();
-            // Check for special tags
-            if (story.currentTags.Contains("MINIGAME_CONDOM_SIZING"))
+    [Header("UI Parents")]
+    [SerializeField] private Transform storyTextParent = null;  // For spawning story text
+    [SerializeField] private Transform choicesParent = null;    // For spawning choices
+
+    [Header("UI Prefabs")]
+    [SerializeField] private Text textPrefab = null;
+    [SerializeField] private Button buttonPrefab = null;
+
+    void Awake()
+    {
+        RemoveChildren();
+        StartStory();
+    }
+
+    void StartStory()
+    {
+        story = new Story(inkJSONAsset.text);
+
+        if (PuttingCondom.nextKnot != "") 
+        {
+            Debug.Log("Choosing path: " + PuttingCondom.nextKnot);
+            story.ChoosePathString(PuttingCondom.nextKnot);
+            PuttingCondom.nextKnot = "";
+        }
+
+        OnCreateStory?.Invoke(story);
+
+        RefreshView();
+    }
+
+    void RefreshView()
+    {
+        RemoveChildren();
+
+        while (story.canContinue)
+        {
+            string text = story.Continue().Trim();
+
+            // Handle special tags
+            HandleTags();
+
+            CreateContentView(text);
+        }
+
+        // Handle choices
+        if (story.currentChoices.Count > 0)
+        {
+            foreach (Choice choice in story.currentChoices)
             {
-                StartCondomSizingMiniGame();
-                return; 
+                Button choiceButton = CreateChoiceView(choice.text.Trim());
+                choiceButton.onClick.AddListener(delegate {
+                    OnClickChoiceButton(choice);
+                });
             }
-			if (story.currentTags.Contains("SWITCH_BACKGROUND_CLINIC"))
-			{
-				FindObjectOfType<BackgroundManagerScript>().SwitchToClinic();
-			}
-				// This removes any white space from the text.
-			text = text.Trim();
-			// Display the text on screen!
-			CreateContentView(text);
-		}
-		
+        }
+        else
+        {
+            Button restartButton = CreateChoiceView("End of story.\nRestart?");
+            restartButton.onClick.AddListener(delegate {
+                StartStory();
+            });
+        }
+    }
 
-		// Display all the choices, if there are any!
-		if(story.currentChoices.Count > 0) {
-			for (int i = 0; i < story.currentChoices.Count; i++) {
-				Choice choice = story.currentChoices [i];
-				Button button = CreateChoiceView (choice.text.Trim ());
-				// Tell the button what to do when we press it
-				button.onClick.AddListener (delegate {
-					OnClickChoiceButton (choice);
-				});
-			}
-		}
-		// If we've read all the content and there's no choices, the story is finished!
-		else {
-			Button choice = CreateChoiceView("End of story.\nRestart?");
-			choice.onClick.AddListener(delegate{
-				StartStory();
-			});
-		}
-	}
+    void HandleTags()
+    {
+        if (story.currentTags.Contains("MINIGAME_CONDOM_SIZING"))
+        {
+            StartCondomSizingMiniGame();
+        }
 
-	// When we click the choice button, tell the story to choose that choice!
-	void OnClickChoiceButton (Choice choice) {
-		story.ChooseChoiceIndex (choice.index);
-		RefreshView();
-	}
+        if (story.currentTags.Contains("SWITCH_BACKGROUND_CLINIC"))
+        {
+            FindObjectOfType<BackgroundManagerScript>().SwitchToClinic();
+        }
+    }
 
-	// Creates a textbox showing the the line of text
-	void CreateContentView (string text) {
-		Text storyText = Instantiate (textPrefab) as Text;
-		storyText.text = text;
-		storyText.transform.SetParent (canvas.transform, false);
-	}
+    void OnClickChoiceButton(Choice choice)
+    {
+        story.ChooseChoiceIndex(choice.index);
+        RefreshView();
+    }
 
-	// Creates a button showing the choice text
-	Button CreateChoiceView (string text) {
-		// Creates the button from a prefab
-		Button choice = Instantiate (buttonPrefab) as Button;
-		choice.transform.SetParent (canvas.transform, false);
-		
-		// Gets the text from the button prefab
-		Text choiceText = choice.GetComponentInChildren<Text> ();
-		choiceText.text = text;
+    void CreateContentView(string text)
+    {
+        Text storyText = Instantiate(textPrefab, storyTextParent);
+        storyText.text = text;
+    }
 
-		// Make the button expand to fit the text
-		HorizontalLayoutGroup layoutGroup = choice.GetComponent <HorizontalLayoutGroup> ();
-		layoutGroup.childForceExpandHeight = false;
+    Button CreateChoiceView(string text)
+    {
+        Button choiceButton = Instantiate(buttonPrefab, choicesParent);
+        Text choiceText = choiceButton.GetComponentInChildren<Text>();
+        choiceText.text = text;
 
-		return choice;
-	}
+        // Make choice button not force expand vertically
+        HorizontalLayoutGroup layoutGroup = choiceButton.GetComponent<HorizontalLayoutGroup>();
+        if (layoutGroup != null)
+        {
+            layoutGroup.childForceExpandHeight = false;
+        }
 
-	// Destroys all the children of this gameobject (all the UI)
-	void RemoveChildren () {
-		int childCount = canvas.transform.childCount;
-		for (int i = childCount - 1; i >= 0; --i) {
-			Destroy (canvas.transform.GetChild (i).gameObject);
-		}
-	}
-    
+        return choiceButton;
+    }
+
+    void RemoveChildren()
+    {
+        // Clear story text
+        for (int i = storyTextParent.childCount - 1; i >= 0; i--)
+        {
+            Destroy(storyTextParent.GetChild(i).gameObject);
+        }
+
+        // Clear choices
+        for (int i = choicesParent.childCount - 1; i >= 0; i--)
+        {
+            Destroy(choicesParent.GetChild(i).gameObject);
+        }
+    }
+
     void StartCondomSizingMiniGame()
     {
         Debug.Log("Launching Condom Sizing Mini-Game...");
-        // Example: load another scene
-        SceneManager.LoadScene("IremsuScene");
+        SceneManager.LoadScene("IremsuScene");  // Example scene name
     }
-
-
-	[SerializeField]
-	private TextAsset inkJSONAsset = null;
-	public Story story;
-
-	[SerializeField]
-	private Canvas canvas = null;
-
-	// UI Prefabs
-	[SerializeField]
-	private Text textPrefab = null;
-	[SerializeField]
-	private Button buttonPrefab = null;
 }
